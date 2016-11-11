a136143e554cbf7d56ec456bc93b7f187ac5266a
Pramod Sadalage <pramodsadalage@gmail.com>
2016-11-11 09:25:37 -0600

    Updated the usage

--- usage/usage_equipment.sql
+++ usage/usage_equipment.sql
@@ -1,4 +1,8 @@
-INSERT INTO equipment (equipmentid,description,starting_miles,service_interval_miles,service_interval_duration,service_interval_duration_unit,acquired_date,acquired_by,acquired_from,serial_number,notes,manufacturer,equipmentstatus,manufacturerid,equipmentstatusid,fueltypeid) values (s_equipment.nextval,'TEST-ME-TO-ACCESS',0,0,6,'Months',SYSDATE,'TEST','TEST','TEST_ME_TOO_ACCESS','Light','MANN','New',7,1,2)
+INSERT INTO equipment (equipmentid,description,starting_miles,service_interval_miles,service_interval_duration, service_interval_duration_unit,acquired_date,acquired_by,acquired_from,serial_number,notes, manufacturer,equipmentstatus,manufacturerid,equipmentstatusid,fueltypeid,location)
+VALUES
+(s_equipment.nextval,'TEST-ME-TO-ACCESS',0,0,6,'Months',
+SYSDATE,'TEST', 'TEST','TEST_ME_TOO_ACCESS','Light','MANN','New',7,1,2,
+'202 E Randolph St. Chicago IL 60601')
 ;
 
 SELECT
